8c32d1e50b5b754b131f9826ee05a36bde6146b2
levisstrauss <levisstrauss11@yahoo.com>
2024-05-20 12:36:11 -0400

    Implemented Email Service

---
+++ src/main/java/com/codemon/securedoc/service/EmailService.java
@@ -0,0 +1,6 @@
+package com.codemon.securedoc.service;
+
+public interface EmailService {
+    void sendNewAccountEmail(String name, String email, String token);
+    void sendPasswordResetEmail(String name, String email, String token);
+}
